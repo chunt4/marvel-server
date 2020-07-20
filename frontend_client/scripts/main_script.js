c4558e9f4294c1b2f7f4424a30b1354d1c18edb4
andrew.rocks <arocks@nd.edu>
2020-07-19 20:12:40 -0400

    added filtering button

--- frontend_client/scripts/main_script.js
+++ frontend_client/scripts/main_script.js
@@ -1,13 +1,25 @@
 // main_script.js
+console.log("started main_script");
 
 // add reaction to Search button
 search_button = document.getElementById("search-button");
 search_button.onmouseup = makeSearchRequest;
 
+// add reaction to apply filter button
+filter_button = document.getElementById("apply-filter");
+filter_button.onmouseup = applyFilter;
+
+// function to make a search query when search button clicked
 function makeSearchRequest() {
     console.log("started make search request");
 
-    var url = "http://student04.cse.nd.edu:51031/"
+    var url = "http://student04.cse.nd.edu:51031/";
+    var xhr = new XMLHttpRequest();
 
+    // xhr.open()
+
+}
+
+function applyFilter() {
     
 }
